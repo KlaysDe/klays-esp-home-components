a50adc4dc9c73b680a375a655e65bb17ac4fa7a8
Sebastian Chmiel <sebastian@chmiel.io>
2023-11-22 00:28:01 +0100

    remove extra millis h file

--- esphome/components/key_press_event_sensor/key_press_event_sensor.h
+++ esphome/components/key_press_event_sensor/key_press_event_sensor.h
@@ -6,7 +6,6 @@
 #include "esphome/components/text_sensor/text_sensor.h"
 #include "esphome/core/log.h"
 
-#include "elapsedMillis.h"
 #include <usb/usb_host.h>
 #include "show_desc.hpp"
 #include "usbhhelp.hpp"
@@ -24,6 +23,207 @@ extern "C" {
 
 namespace esphome {
 namespace key_press_event_sensor {
+
+class elapsedMicros {
+ private:
+  unsigned long us;
+
+ public:
+  elapsedMicros(void) { us = micros(); }
+  elapsedMicros(unsigned long val) { us = micros() - val; }
+  elapsedMicros(const elapsedMicros &orig) { us = orig.us; }
+  operator unsigned long() const { return micros() - us; }
+  elapsedMicros &operator=(const elapsedMicros &rhs) {
+    us = rhs.us;
+    return *this;
+  }
+  elapsedMicros &operator=(unsigned long val) {
+    us = micros() - val;
+    return *this;
+  }
+  elapsedMicros &operator-=(unsigned long val) {
+    us += val;
+    return *this;
+  }
+  elapsedMicros &operator+=(unsigned long val) {
+    us -= val;
+    return *this;
+  }
+  elapsedMicros operator-(int val) const {
+    elapsedMicros r(*this);
+    r.us += val;
+    return r;
+  }
+  elapsedMicros operator-(unsigned int val) const {
+    elapsedMicros r(*this);
+    r.us += val;
+    return r;
+  }
+  elapsedMicros operator-(long val) const {
+    elapsedMicros r(*this);
+    r.us += val;
+    return r;
+  }
+  elapsedMicros operator-(unsigned long val) const {
+    elapsedMicros r(*this);
+    r.us += val;
+    return r;
+  }
+  elapsedMicros operator+(int val) const {
+    elapsedMicros r(*this);
+    r.us -= val;
+    return r;
+  }
+  elapsedMicros operator+(unsigned int val) const {
+    elapsedMicros r(*this);
+    r.us -= val;
+    return r;
+  }
+  elapsedMicros operator+(long val) const {
+    elapsedMicros r(*this);
+    r.us -= val;
+    return r;
+  }
+  elapsedMicros operator+(unsigned long val) const {
+    elapsedMicros r(*this);
+    r.us -= val;
+    return r;
+  }
+};
+
+class elapsedMillis {
+ private:
+  unsigned long ms;
+
+ public:
+  elapsedMillis(void) { ms = millis(); }
+  elapsedMillis(unsigned long val) { ms = millis() - val; }
+  elapsedMillis(const elapsedMillis &orig) { ms = orig.ms; }
+  operator unsigned long() const { return millis() - ms; }
+  elapsedMillis &operator=(const elapsedMillis &rhs) {
+    ms = rhs.ms;
+    return *this;
+  }
+  elapsedMillis &operator=(unsigned long val) {
+    ms = millis() - val;
+    return *this;
+  }
+  elapsedMillis &operator-=(unsigned long val) {
+    ms += val;
+    return *this;
+  }
+  elapsedMillis &operator+=(unsigned long val) {
+    ms -= val;
+    return *this;
+  }
+  elapsedMillis operator-(int val) const {
+    elapsedMillis r(*this);
+    r.ms += val;
+    return r;
+  }
+  elapsedMillis operator-(unsigned int val) const {
+    elapsedMillis r(*this);
+    r.ms += val;
+    return r;
+  }
+  elapsedMillis operator-(long val) const {
+    elapsedMillis r(*this);
+    r.ms += val;
+    return r;
+  }
+  elapsedMillis operator-(unsigned long val) const {
+    elapsedMillis r(*this);
+    r.ms += val;
+    return r;
+  }
+  elapsedMillis operator+(int val) const {
+    elapsedMillis r(*this);
+    r.ms -= val;
+    return r;
+  }
+  elapsedMillis operator+(unsigned int val) const {
+    elapsedMillis r(*this);
+    r.ms -= val;
+    return r;
+  }
+  elapsedMillis operator+(long val) const {
+    elapsedMillis r(*this);
+    r.ms -= val;
+    return r;
+  }
+  elapsedMillis operator+(unsigned long val) const {
+    elapsedMillis r(*this);
+    r.ms -= val;
+    return r;
+  }
+};
+
+class elapsedSeconds {
+ private:
+  unsigned long s;
+
+ public:
+  elapsedSeconds(void) { s = millis() / 1000; }
+  elapsedSeconds(unsigned long val) { s = millis() / 1000 - val; }
+  elapsedSeconds(const elapsedSeconds &orig) { s = orig.s; }
+  operator unsigned long() const { return millis() / 1000 - s; }
+  elapsedSeconds &operator=(const elapsedSeconds &rhs) {
+    s = rhs.s;
+    return *this;
+  }
+  elapsedSeconds &operator=(unsigned long val) {
+    s = millis() / 1000 - val;
+    return *this;
+  }
+  elapsedSeconds &operator-=(unsigned long val) {
+    s += val;
+    return *this;
+  }
+  elapsedSeconds &operator+=(unsigned long val) {
+    s -= val;
+    return *this;
+  }
+  elapsedSeconds operator-(int val) const {
+    elapsedSeconds r(*this);
+    r.s += val;
+    return r;
+  }
+  elapsedSeconds operator-(unsigned int val) const {
+    elapsedSeconds r(*this);
+    r.s += val;
+    return r;
+  }
+  elapsedSeconds operator-(long val) const {
+    elapsedSeconds r(*this);
+    r.s += val;
+    return r;
+  }
+  elapsedSeconds operator-(unsigned long val) const {
+    elapsedSeconds r(*this);
+    r.s += val;
+    return r;
+  }
+  elapsedSeconds operator+(int val) const {
+    elapsedSeconds r(*this);
+    r.s -= val;
+    return r;
+  }
+  elapsedSeconds operator+(unsigned int val) const {
+    elapsedSeconds r(*this);
+    r.s -= val;
+    return r;
+  }
+  elapsedSeconds operator+(long val) const {
+    elapsedSeconds r(*this);
+    r.s -= val;
+    return r;
+  }
+  elapsedSeconds operator+(unsigned long val) const {
+    elapsedSeconds r(*this);
+    r.s -= val;
+    return r;
+  }
+};
 
 class KeyPressEventSensor : public text_sensor::TextSensor, public Component {
  private:
