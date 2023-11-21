83cf65ae1e9aed1ec2fe0e673175b9601bf64395
Sebastian Chmiel <sebastian@chmiel.io>
2023-11-21 23:29:18 +0100

    Try fix text sensor

--- esphome/components/key_press_event_sensor/text_sensor.py
+++ esphome/components/key_press_event_sensor/text_sensor.py
@@ -1,20 +1,29 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import text_sensor
-from esphome.const import CONF_ID
+from esphome.components import text_sensor,sensor
+from esphome.const import (
+    CONF_SENSOR_ID,
+    CONF_THRESHOLD,
+)
 
 CODEOWNERS = ["@kpx1"]
 DEPENDENCIES = ["esp32"]
 
 empty_text_sensor_ns = cg.esphome_ns.namespace('key_press_event_sensor')
-EmptyTextSensor = empty_text_sensor_ns.class_('KeyPressEventSensor', text_sensor.TextSensor, cg.Component)
+keyboardSensorEntity = empty_text_sensor_ns.class_('KeyPressEventSensor', text_sensor.TextSensor, cg.Component)
 
-CONFIG_SCHEMA = text_sensor.TEXT_SENSOR_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(EmptyTextSensor)
-}).extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = (text_sensor
+                .text_sensor_schema(keyboardSensorEntity)
+                .extend({
+                    cv.Required(CONF_SENSOR_ID): cv.use_id(sensor.Sensor),
+                })
+                .extend(cv.COMPONENT_SCHEMA)
+)
 
 
-def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    yield text_sensor.register_text_sensor(var, config)
-    yield cg.register_component(var, config)
+async def to_code(config):
+    var = await text_sensor.new_text_sensor(config)
+    cg.register_component(var, config)
+    
+    sens = await cg.get_variable(config[CONF_SENSOR_ID])
+    cg.add(var.set_sensor(sens))
